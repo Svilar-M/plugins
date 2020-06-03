e2aed885aefd092cb40fd70994d2c659c9784739
Svilar-M <milicasvilarnew@gmail.com>
2020-06-03 11:10:10 +0200

    test
    ...

--- toys.js
+++ toys.js
@@ -1,8 +1,6 @@
-doSomething = (product) => {
-  console.log('This is console.log from function doSomthing()');
-  console.log(product);
-  return 'yooopy';
-}
-export default { doSomething };
-
-doSomething(null);
+    export default function doSomething(product) {
+      console.log('This is console.log from function doSomthing()');
+      console.log(product);
+      return 'yooopy';
+    }
+    doSomething(null);
